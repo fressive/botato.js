ae29cd63a619bcf5580abfe690027b1683758315
int100 <me@int100.org>
2019-02-11 21:29:46 +0800

    [M] wahtani can show the cover

--- src/plugins/wahtani.ts
+++ src/plugins/wahtani.ts
@@ -3,8 +3,8 @@
 // Author: int100
 
 import * as ionjs from '@ionjs/core'
-import { IMessage, Utils } from '@ionjs/core';
-import axios, { AxiosResponse } from 'axios'
+import { Utils, Codes } from '@ionjs/core';
+import axios from 'axios'
 
 /**
  *  Format seconds to "x分 y秒".
@@ -15,6 +15,32 @@ function secondFormat(second: number): string {
     var s = sec % 60
     var m = (sec - s) / 60
     return `${m}分 ${s}秒`
+}
+
+/**
+ *  Get cover url from anilist id
+ *  @param id Id in anilist.co
+ */
+async function getCover(id: number): Promise<string> {
+
+    return new Promise(async function(resolve, reject) {
+        try {
+            var query = "query ($id: Int) { Media (id: $id, type: ANIME) { coverImage { large } } }"
+            var rep = await axios.post(
+                    "https://graphql.anilist.co", 
+                    JSON.stringify({query: query, variables: { id: id }}), 
+                    { headers: { "Content-Type": "application/json" } }
+                )
+            
+            var image = rep.data.data.Media.coverImage.large
+            if (image)
+                resolve(image)
+            else 
+                reject("not exists")
+        } catch (err) {
+            reject(err)
+        }
+    })
 }
 
 ionjs.useSession(ionjs.when.contain("wahtani"), { concurrent: true }) (
@@ -36,7 +62,7 @@ ionjs.useSession(ionjs.when.contain("wahtani"), { concurrent: true }) (
         var base64 = Buffer.from(img[0].data).toString('base64');
 
         var animeRep = await axios.post(
-                "https://wait.hirina.cn/api/search",  // use the proxy site of trace.moe because it's blocked in China :(
+                "https://wait.hirina.cn/api/search",  // use the proxy site of trace.moe because source is blocked in China :(
                 JSON.stringify({image: base64}), 
                 { headers: { "Content-Type": "application/json" } }
         )
@@ -44,12 +70,19 @@ ionjs.useSession(ionjs.when.contain("wahtani"), { concurrent: true }) (
         var animes = animeRep.data.docs.filter((it) => it.similarity > 0.8)
 
         if (animes.length == 0) {
-            await ctx.reply("未找到纸片人出处或是非番图。")
+            await ctx.reply("未找到纸片人出处或是非番图（画面越完整越好）。")
             return
         } else {
             var anime = animes[0]
-            await ctx.reply(`原名：${anime.title_native}\n译名：${anime.anime} \nID: ${anime.anilist_id}\n在 Ep.${anime.episode} ${secondFormat(anime.at)} 处出现。`)
-            return
+            var text = `原名：${anime.title_native}\n译名：${anime.anime} \n在 Ep.${anime.episode} ${secondFormat(anime.at)} 处出现。`
+            
+            if (!anime.is_adult) { // disable NSFW in QQ Groups
+                await ctx.reply(Codes.Image(await getCover(anime.anilist_id)), "\n", text)
+            } else {
+                await ctx.reply(text)
+                return
+            }
+
         }
 
     }
